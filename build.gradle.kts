c3f1fed790eb332db5f589c928110626df9469b3
KiARC <katiearose@protonmail.com>
2022-03-16 08:47:50 -0400

    chore: initialize repository
    Set up the repo. Using LGPLv3+GPLv3 licensing.
    
    Signed-off-by: KiARC <katiearose@protonmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+    java
+}
+
+group = "com.katiearose"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation(kotlin("test-junit5"))
+    testImplementation(platform("org.junit:junit-bom:5.7.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
