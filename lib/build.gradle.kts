0e0b8ea2d9e4dcbc4cdbb02537e90f53b6689cda
KiARC <katiearose@protonmail.com>
2022-03-18 11:39:50 -0400

    build: set up Gradle to build JARs
    Set up build.gradle.kts to be able to build useful library JARs

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,26 +1,40 @@
-plugins {
-    `java-library`
-    kotlin("jvm") version "1.6.10"
-    java
-}
+group = "com.katiearose"
+version = "0.4.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation(kotlin("stdlib"))
     testImplementation(kotlin("test-junit5"))
     testImplementation(platform("org.junit:junit-bom:5.7.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
-group = "com.katiearose"
-version = "v0.1.0"
+plugins {
+    `java-library`
+    kotlin("jvm") version "1.6.10"
+    java
+    `maven-publish`
+    application
+}
 
 tasks.test {
     useJUnitPlatform()
     testLogging {
         events("passed", "skipped", "failed")
     }
+}
+
+tasks.jar {
+    dependsOn("test")
+    manifest {
+        attributes(
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version
+        )
+    }
+    baseName = "REC"
 }
