2a096edb8df35094408abd5000647361b08fb329
KiARC <katiearose@protonmail.com>
2022-03-18 17:30:53 -0400

    test: make tests hit 100% coverage in EncryptionHandlers

--- src/test/kotlin/reasonablyEasyCryptography/AsymmetricEncryptionTest.kt
+++ src/test/kotlin/reasonablyEasyCryptography/AsymmetricEncryptionTest.kt
@@ -2,6 +2,7 @@ package reasonablyEasyCryptography
 
 import org.junit.jupiter.api.Test
 import java.util.*
+import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 class AsymmetricEncryptionTest {
@@ -44,5 +45,65 @@ class AsymmetricEncryptionTest {
         val decrypted =
             AsymmetricEncryptionHandler.decryptAndVerify(encrypted.data, encrypted.signature, keys.private, keys.public)
         assertTrue(goal.contentEquals(decrypted))
+    }
+
+    @Test
+    fun testAllWithInvalidSig() {
+        val keys = AsymmetricEncryptionHandler.generateKeyPair()
+        val goal = ByteArray(1024)
+        Random().nextBytes(goal)
+        val fakeKey = ByteArray(256)
+        Random().nextBytes(fakeKey)
+        val encrypted = AsymmetricEncryptionHandler.encryptAndSign(goal, keys.public, keys.private)
+        val badSig = SignedDataContainer(encrypted.data, fakeKey)
+        var failed = false
+        try {
+            val decrypted = AsymmetricEncryptionHandler.decryptAndVerify(badSig, keys.private, keys.public)
+        } catch (e: SecurityException) {
+            failed = true
+        }
+        assertTrue(failed)
+    }
+
+    @Test
+    fun testAll2WithInvalidSig() {
+        val keys = AsymmetricEncryptionHandler.generateKeyPair()
+        val goal = ByteArray(1024)
+        Random().nextBytes(goal)
+        val fakeKey = ByteArray(256)
+        Random().nextBytes(fakeKey)
+        val encrypted = AsymmetricEncryptionHandler.encryptAndSign(goal, keys.public, keys.private)
+        var failed = false
+        try {
+            val decrypted = AsymmetricEncryptionHandler.decryptAndVerify(encrypted.data, fakeKey, keys.private, keys.public)
+        } catch (e: SecurityException) {
+            failed = true
+        }
+        assertTrue(failed)
+    }
+
+    @Test
+    fun testAllWithInvalidSig2() {
+        val keys = AsymmetricEncryptionHandler.generateKeyPair()
+        val goal = ByteArray(1024)
+        Random().nextBytes(goal)
+        val fakeKey = ByteArray(256)
+        Random().nextBytes(fakeKey)
+        val encrypted = AsymmetricEncryptionHandler.encryptAndSign(goal, keys.public, keys.private)
+        val badSig = SignedDataContainer(encrypted.data, fakeKey)
+        val decrypted = AsymmetricEncryptionHandler.decryptAndVerify(badSig, keys.private, keys.public, false)
+        assertNull(decrypted)
+    }
+
+    @Test
+    fun testAll2WithInvalidSig2() {
+        val keys = AsymmetricEncryptionHandler.generateKeyPair()
+        val goal = ByteArray(1024)
+        Random().nextBytes(goal)
+        val fakeKey = ByteArray(256)
+        Random().nextBytes(fakeKey)
+        val encrypted = AsymmetricEncryptionHandler.encryptAndSign(goal, keys.public, keys.private)
+        val decrypted = AsymmetricEncryptionHandler.decryptAndVerify(encrypted.data, fakeKey, keys.private, keys.public, false)
+        assertNull(decrypted)
     }
 }
--- src/test/kotlin/reasonablyEasyCryptography/SymmetricEncryptionTest.kt
+++ src/test/kotlin/reasonablyEasyCryptography/SymmetricEncryptionTest.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertTrue
 class SymmetricEncryptionTest {
     @Test
     fun testEncryptAndDecryptWithKey() {
-        val key = SymmetricEncryptionHandler.stringToKey("test")
+        val key = SymmetricEncryptionHandler.generateKey()
         val goal = ByteArray(1024)
         Random().nextBytes(goal)
         val encrypted = SymmetricEncryptionHandler.encrypt(goal, key)
@@ -22,5 +22,11 @@ class SymmetricEncryptionTest {
         val encrypted = SymmetricEncryptionHandler.encrypt(goal, "test")
         val decrypted = SymmetricEncryptionHandler.decrypt(encrypted, "test")
         assertTrue(goal.contentEquals(decrypted))
+    }
+
+    @Test fun testKeyGen() {
+        val k1 = SymmetricEncryptionHandler.generateKey()
+        val k2 = SymmetricEncryptionHandler.generateKey(128)
+        val k3 = SymmetricEncryptionHandler.stringToKey("test")
     }
 }
