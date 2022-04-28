8e0580f1c976e8fd7607277ccff92a341563d9b5
KiARC <katiearose@protonmail.com>
2022-04-27 20:36:20 -0400

    feat: add support for multiple recipients
    write methods to encrypt a message for multiple recipients without massively increasing ciphertext
    size, modify existing methods to work with the slight modification to the message format (first byte
    now represents the number of recipient keys so the decryptor can know where to look), add an
    override for encryptAndSign to handle a list of keys
    
    BREAKING CHANGE: new message format, which consists of an extra byte at the start of the message to
    indicate the number of recipients. Messages from previous versions of REC can be decrypted with
    legacyDecrypt(message, key). This method will be removed with the next major version.
    
    complete #1

--- src/main/kotlin/com/katiearose/reasonablyEasyCryptography/asymmetric/RSAEncryptionHandler.kt
+++ src/main/kotlin/com/katiearose/reasonablyEasyCryptography/asymmetric/RSAEncryptionHandler.kt
@@ -3,7 +3,9 @@ package com.katiearose.reasonablyEasyCryptography.asymmetric
 import com.katiearose.reasonablyEasyCryptography.symmetric.AESEncryptionHandler
 import java.nio.ByteBuffer
 import java.security.*
+import javax.crypto.BadPaddingException
 import javax.crypto.Cipher
+import javax.crypto.SecretKey
 
 object RSAEncryptionHandler {
     /**
@@ -38,8 +40,43 @@ object RSAEncryptionHandler {
         val c = Cipher.getInstance("RSA/ECB/OAEPwithSHA-256andMGF1Padding")
         c.init(Cipher.ENCRYPT_MODE, key)
         val kEnc = c.doFinal(k.encoded)
-        val output = ByteBuffer.allocate(kEnc.size + encryptedData.size)
+        val output = ByteBuffer.allocate(kEnc.size + encryptedData.size + 1)
+        output.put(0)
         output.put(kEnc)
+        output.put(encryptedData)
+        return output.array()
+    }
+
+    /**
+     * Encrypts data with a given list of up to 256 public keys
+     *
+     * The encrypted data can be decrypted by the private counterpart to any of the keys in the list
+     *
+     * The limit of 256 is imposed by the fact that I am using a single byte to tell the decryptor how many recipients were specified. It should be enough for most use cases.
+     *
+     * @author Katherine Rose
+     * @param data the data to encrypt
+     * @param keys the keys to use for encryption
+     * @return the encrypted data and its encrypted symmetric keys
+     */
+    @JvmStatic
+    fun encrypt(data: ByteArray, keys: List<PublicKey>): ByteArray {
+        if (keys.size > 256) {
+            throw RuntimeException("More than 256 recipients specified at once")
+        }
+        val encryptedSecrets = ArrayList<ByteArray>(keys.size)
+        val k = AESEncryptionHandler.generateKey(128)
+        val encryptedData = AESEncryptionHandler.encrypt(data, k)
+        for (key in keys) {
+            val c = Cipher.getInstance("RSA/ECB/OAEPwithSHA-256andMGF1Padding")
+            c.init(Cipher.ENCRYPT_MODE, key)
+            encryptedSecrets.add(c.doFinal(k.encoded))
+        }
+        var totalSize = 0
+        for (s in encryptedSecrets) totalSize += s.size
+        val output = ByteBuffer.allocate(totalSize + encryptedData.size + 1)
+        output.put((keys.size.toUByte()).toByte()) //Janky math to hopefully make this work
+        for (s in encryptedSecrets) output.put(s)
         output.put(encryptedData)
         return output.array()
     }
@@ -56,6 +93,61 @@ object RSAEncryptionHandler {
      */
     @JvmStatic
     fun decrypt(data: ByteArray, key: PrivateKey): ByteArray {
+        val c = Cipher.getInstance("RSA/ECB/OAEPwithSHA-256andMGF1Padding")
+        c.init(Cipher.DECRYPT_MODE, key)
+        val buffer = ByteBuffer.wrap(data)
+        val rbyte = ByteArray(1)
+        buffer.get(rbyte)
+        val recipientCount = rbyte[0].toUByte()
+        if (recipientCount == 0.toUByte()) {
+            val kB = ByteArray(256)
+            buffer.get(kB)
+            val enc = ByteArray(buffer.remaining())
+            buffer.get(enc)
+            val k = AESEncryptionHandler.assembleKey(c.doFinal(kB))
+            return AESEncryptionHandler.decrypt(enc, k)
+        } else {
+            val kbs = ArrayDeque<ByteArray>(recipientCount.toInt())
+            for (i in 0 until recipientCount.toInt()) {
+                val kB = ByteArray(256)
+                buffer.get(kB)
+                kbs.addFirst(kB)
+            }
+            var foundKey = false
+            var k: SecretKey? = null
+            while (!foundKey) {
+                try {
+                    k = AESEncryptionHandler.assembleKey(c.doFinal(kbs.removeFirst()))
+                    foundKey = true
+                } catch (_: BadPaddingException) { /*Ignore that, it just means that wasn't the right one*/
+                }
+            }
+            val enc = ByteArray(buffer.remaining())
+            buffer.get(enc)
+            return AESEncryptionHandler.decrypt(enc, k!!)
+        }
+    }
+
+    /**
+     * Decrypts data with a given private key
+     *
+     * Retrieves the symmetric key from the data, decrypts it and then uses it to decrypt the message
+     *
+     * Legacy method from before REC supported multiple recipients, which came with an extra byte at the beginning of the messages
+     *
+     * @author Katherine Rose
+     * @param data the data to decrypt
+     * @param key the key to use for decryption
+
+     * @return the decrypted data
+     */
+    @JvmStatic
+    @Deprecated(
+        "Only for use on messages encrypted with older versions of REC",
+        ReplaceWith("decrypt()"),
+        DeprecationLevel.WARNING
+    )
+    fun legacyDecrypt(data: ByteArray, key: PrivateKey): ByteArray {
         val c = Cipher.getInstance("RSA/ECB/OAEPwithSHA-256andMGF1Padding")
         c.init(Cipher.DECRYPT_MODE, key)
         val buffer = ByteBuffer.wrap(data)
@@ -107,7 +199,7 @@ object RSAEncryptionHandler {
      * @param data the data to encrypt and sign
      * @param encryptionKey the key to use for encryption
      * @param signingKey the key to use for signing
-     * @return a Pair, in which the first ByteArray is the encrypted data and the second is the signature
+     * @return a SignedDataContainer
      */
     @JvmStatic
     fun encryptAndSign(
@@ -116,6 +208,26 @@ object RSAEncryptionHandler {
         signingKey: PrivateKey
     ): SignedDataContainer {
         val enc = encrypt(data, encryptionKey)
+        val sig = sign(enc, signingKey)
+        return SignedDataContainer(enc, sig)
+    }
+
+    /**
+     * Encrypts and then signs a piece of data with multiple keys
+     *
+     * @author Katherine Rose
+     * @param data the data to encrypt and sign
+     * @param encryptionKeys the keys to use for encryption
+     * @param signingKey the key to use for signing
+     * @return a SignedDataContainer
+     */
+    @JvmStatic
+    fun encryptAndSign(
+        data: ByteArray,
+        encryptionKeys: List<PublicKey>,
+        signingKey: PrivateKey
+    ): SignedDataContainer {
+        val enc = encrypt(data, encryptionKeys)
         val sig = sign(enc, signingKey)
         return SignedDataContainer(enc, sig)
     }
